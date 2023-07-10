65d566d0d50559abab0a61c92210df9973a93a5a
dande100 <135659465+dande100@users.noreply.github.com>
2023-07-10 23:50:20 +0000

    Domain generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+function domainGenerator() {
+  let domains = [];
+  pronoun.forEach(item => {
+    adj.forEach(adjEl => {
+      noun.forEach(nounEl => {
+        domains.push(item + adjEl + nounEl + ".com");
+      });
+    });
+  });
+  return domains;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const el = document.getElementById("domain");
+  const ul = document.createElement("ul");
+  el.appendChild(ul);
+  const arr = domainGenerator();
+  arr.forEach(item => {
+    const li = document.createElement("li");
+    ul.appendChild(li);
+    li.innerHTML = item;
+  });
 };
